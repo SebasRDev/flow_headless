b8c530172cdd75c1e6732d6327dcd58e6e13a09a
Maicol Murcia <developer@iMac-de-Maicol.local>
2023-08-15 13:38:21 -0500

    Updated variants settings

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,16 +1,7 @@
 import React from "react";
-import { cva } from "class-variance-authority";
 import { clsx } from 'clsx';
-import { variants } from "./Button.variants";
+import { button } from "./Button.variants";
 import { ButtonProps } from "./Button.interface";
-
-const button = cva("font-bold rounded-lg", {
-  variants: variants,
-  defaultVariants: {
-    variant: "primary",
-    size: "small",
-  },
-});
 
 export const Button: React.FC<ButtonProps> = ({
   variant,
